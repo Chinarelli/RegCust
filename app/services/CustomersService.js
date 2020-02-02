b44a296d434aaff23c06fbe58107ed176a959811
Chinarelli <lucaschinarelli11@gmail.com>
2020-02-01 21:27:17 -0300

    Ajustado Consulta de Cliente e Tratamento de Erros

--- app/services/CustomersService.js
+++ app/services/CustomersService.js
@@ -9,15 +9,19 @@ class CustormersService {
     const codreg = scope.message.text.replace('/customers', '').replace(/^\s+|\s+$|\s+(?=\s)/g, '');
     if (codreg) {
       const agents = await this.pgConnect.query('SELECT * FROM CLIENTES WHERE cli_reg_codigo = $1', [codreg]);
-      agents.rows.forEach((element) => {
-        datacust = (`${element.cli_codigo} - ${element.cli_fantasia}\n`);
-        if ((message.length + datacust.length) > 4096) {
-          scope.sendMessage(message);
-          message = datacust;
-        } else {
-          message += datacust;
-        }
-      });
+      if (agents.rowCount > 0) {
+        agents.rows.forEach((element) => {
+          datacust = (`${element.cli_codigo} - ${element.cli_fantasia}\n`);
+          if ((message.length + datacust.length) > 4096) {
+            scope.sendMessage(message);
+            message = datacust;
+          } else {
+            message += datacust;
+          }
+        });
+      } else {
+        message = 'Informed Regions Was Not Found';
+      }
     } else {
       message = 'Search field Not Informed';
     }
@@ -29,28 +33,38 @@ class CustormersService {
   async findagentcust(scope) {
     let message = '';
     let datacust = '';
+    let datarep = '';
     const descagent = scope.message.text.replace('/agentcust', '').trim();
     if (descagent) {
       const agents = await this.pgConnect.query('SELECT C.cli_codigo, C.cli_fantasia, R.reg_codigo, R.reg_descricao '
                                               + 'FROM CLIENTES AS C JOIN REGIOES AS R ON C.cli_reg_codigo = R.reg_codigo '
                                               + 'WHERE LOWER(cli_fantasia) like LOWER($1) ORDER BY R.reg_codigo', [`%${descagent}%`]);
-
-      agents.rows.forEach((element) => {
-        datacust = (`${element.cli_codigo} - ${element.cli_fantasia}\n`);
-        datacust += (`${element.reg_codigo} - ${element.reg_descricao}\n\n`);
-        if ((message.length + datacust.length) > 4096) {
-          scope.sendMessage(message);
-          message = datacust;
-        } else {
-          message += datacust;
-        }
-      });
+      let codrep = null;
+      const formatcust = '========= Customers =========';
+      if (agents.rowCount > 0) {
+        agents.rows.forEach((element) => {
+          if (codrep !== element.reg_codigo) {
+            datarep = (`\nRepresentante: ${element.reg_codigo} - ${element.reg_descricao}\n`);
+            datacust = (`${datarep}${formatcust}\n${element.cli_codigo} - ${element.cli_fantasia}\n`);
+          } else {
+            datarep = '';
+            datacust = (`${element.cli_codigo} - ${element.cli_fantasia}\n`);
+          }
+          codrep = element.reg_codigo;
+          if ((message.length + datacust.length) > 4096) {
+            scope.sendMessage(message);
+            message = datacust;
+          } else {
+            message += datacust;
+          }
+        });
+      } else {
+        message = 'Informed Customer Was Not Found';
+      }
     } else {
       message = 'Search field Not Informed';
     }
     scope.sendMessage(message);
-
-    // this.pgConnect.end();
   }
 }
 
